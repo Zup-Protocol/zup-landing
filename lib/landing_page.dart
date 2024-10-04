316997886ec2d3c9435b1190f7c51a8bced9a0c2
Ryan Holanda <ryanholanda08@gmail.com>
2024-10-04 18:11:04 -0300

    Revert "feat: add mobile support for landing page" (#3)

--- lib/landing_page.dart
+++ lib/landing_page.dart
@@ -12,131 +12,102 @@ class LandingPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        children: [
-          background(),
-          CustomScrollView(
+    return LayoutBuilder(builder: (context, constraints) {
+      return SelectionArea(
+        child: Scaffold(
+          body: CustomScrollView(
             slivers: [
+              const SliverPadding(
+                padding: EdgeInsets.only(top: 40),
+                sliver: SliverToBoxAdapter(
+                  child: Text(
+                    "Your very first liquidity pool aggregator",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: ZupColors.brand,
+                    ),
+                  ),
+                ),
+              ),
               SliverFillRemaining(
                 hasScrollBody: false,
-                fillOverscroll: false,
-                child: Center(
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Assets.logos.zup.svg(height: 250),
-                      const SizedBox(height: 50),
-                      ZupPrimaryButton(
-                        title: "Waitlist",
-                        onPressed: () {},
-                        fixedIcon: true,
-                        width: 150,
-                        padding: EdgeInsets.zero,
-                        icon: Assets.icons.listClipboard.svg(),
-                      ),
-                      const SizedBox(height: 10),
-                      ZupPrimaryButton(
-                        title: "Docs",
-                        hoverElevation: 0,
-                        border: const BorderSide(color: ZupColors.brand),
-                        backgroundColor: Colors.transparent,
-                        foregroundColor: ZupColors.brand,
-                        onPressed: () {},
-                        fixedIcon: true,
-                        icon: Assets.icons.textPage.svg(),
-                        width: 150,
-                      ),
-                      const SizedBox(height: 10),
-                      ZupIconButton(
-                        // backgroundColor: Colors.transparent,
-                        borderSide: const BorderSide(color: ZupColors.brand),
-                        padding: const EdgeInsets.all(12),
-                        iconColor: ZupColors.black,
-                        icon: Assets.logos.x.svg(
-                          height: 22,
+                child: Stack(
+                  alignment: Alignment.topCenter,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.symmetric(
+                              horizontal: horizontalScreenPadding)
+                          .copyWith(top: 40),
+                      child: Assets.logos.zupTyped.svg(height: 500),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 70),
+                      child: Assets.logos.zup.svg(height: 350),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(
+                        horizontal: horizontalScreenPadding,
+                      ).copyWith(bottom: 130),
+                      child: Align(
+                        alignment: Alignment.bottomCenter,
+                        child: ZupPrimaryButton(
+                          title: "Whitelist",
+                          onPressed: () {},
+                          width: isMobile(constraints) ? double.infinity : 200,
+                          height: 60,
+                          padding: EdgeInsets.zero,
+                          fixedIcon: true,
+                          icon: Assets.icons.listClipboard.svg(),
                         ),
-                        onPressed: () => launchUrl(
-                          Uri.parse(
-                            "https://x.com/zup_protocol",
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(
+                        horizontal: horizontalScreenPadding,
+                      ).copyWith(bottom: 60),
+                      child: Align(
+                        alignment: Alignment.bottomCenter,
+                        child: ZupPrimaryButton(
+                          width: isMobile(constraints) ? double.infinity : 200,
+                          height: 60,
+                          title: "Whitepaper",
+                          padding: const EdgeInsets.all(0),
+                          onPressed: () => launchUrl(
+                            Uri.parse(
+                                "https://zupprotocol.notion.site/Zup-Whitepaper-Business-1135a07f86f6809f94b3dbe1192a338d"),
+                          ),
+                          fixedIcon: true,
+                          backgroundColor: Colors.transparent,
+                          border: const BorderSide(color: ZupColors.brand),
+                          foregroundColor: ZupColors.brand,
+                          hoverElevation: 0,
+                          icon: Assets.icons.textPage.svg(),
+                        ),
+                      ),
+                    ),
+                    Align(
+                      alignment: Alignment.bottomRight,
+                      child: Padding(
+                        padding: const EdgeInsets.all(20),
+                        child: ZupIconButton(
+                          padding: const EdgeInsets.all(20),
+                          icon: Assets.logos.x.svg(height: 20),
+                          backgroundColor: ZupColors.gray6,
+                          onPressed: () => launchUrl(
+                            Uri.parse("https://x.com/zup_protocol"),
                           ),
                         ),
-                      )
-                    ],
-                  ),
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ],
           ),
-        ],
-      ),
-    );
+        ),
+      );
+    });
   }
-
-  Widget background() => LayoutBuilder(builder: (context, constraints) {
-        final screenWidth = constraints.maxWidth;
-
-        return Stack(
-          children: [
-            Positioned(
-              left: screenWidth * 0.02,
-              top: screenWidth * 0.02,
-              child: ShaderMask(
-                shaderCallback: (bounds) => const LinearGradient(
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter,
-                  colors: [ZupColors.brand5, ZupColors.brand],
-                ).createShader(
-                  Rect.fromLTWH(0, 0, bounds.width, bounds.height),
-                ),
-                blendMode: BlendMode.srcIn,
-                child: Text(
-                  "Your very first\nliquidity pool\naggregator",
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: (screenWidth * 0.05).clamp(0, 60),
-                  ),
-                ),
-              ),
-            ),
-            Positioned(
-              right: -(screenWidth * 0.1),
-              top: -(screenWidth * 0.02),
-              child: ShaderMask(
-                shaderCallback: (bounds) => const LinearGradient(
-                  colors: [ZupColors.brand, ZupColors.brand, ZupColors.black],
-                ).createShader(
-                  Rect.fromLTWH(0, 0, bounds.width, bounds.height),
-                ),
-                child: Assets.logos.zupTyped.svg(
-                  height: (screenWidth * 0.2).clamp(0, 400),
-                ),
-              ),
-            ),
-            Positioned(
-              left: -(screenWidth * 0.1),
-              bottom: -(screenWidth * 0.02),
-              child: Assets.logos.zupTyped.svg(
-                height: (screenWidth * 0.3).clamp(0, 400),
-                colorFilter: const ColorFilter.mode(
-                  ZupColors.gray5,
-                  BlendMode.srcIn,
-                ),
-              ),
-            ),
-            Positioned(
-              right: screenWidth * 0.02,
-              bottom: screenWidth * 0.02,
-              child: Text(
-                "Smart\nLiquidity\nChoices",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  color: ZupColors.gray5,
-                  fontSize: (screenWidth * 0.05).clamp(0, 60),
-                ),
-              ),
-            ),
-          ],
-        );
-      });
 }
