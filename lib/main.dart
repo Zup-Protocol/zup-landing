09acd9788f0cf2b20e53c08fa5ae85f9c10f93b8
Ryan Holanda <ryanholanda08@gmail.com>
2024-10-17 21:16:47 -0300

    feat: add zup genesis NFT button

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:zup_landing/landing_page.dart';
+import 'package:routefly/routefly.dart';
+import 'package:url_strategy/url_strategy.dart';
 import 'package:zup_landing/landing_theme.dart';
+import 'package:zup_landing/routes.g.dart';
 
 void main() {
+  setPathUrlStrategy();
   runApp(const MainApp());
 }
 
@@ -11,10 +14,20 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       theme: LandingTheme.light,
       debugShowCheckedModeBanner: false,
-      home: const LandingPage(),
+      routerConfig: Routefly.routerConfig(
+        routes: routes,
+        initialPath: routePaths.path,
+        routeBuilder: (context, settings, child) => PageRouteBuilder(
+            pageBuilder: (_, __, ___) => child,
+            settings: settings,
+            transitionsBuilder: (_, a, ___, child) => FadeTransition(
+                  opacity: a,
+                  child: child,
+                )),
+      ),
     );
   }
 }
