1d173f680e9d7006155aaa526b305b4d7c06a120
Ryan Holanda <ryanholanda08@gmail.com>
2024-10-17 23:00:41 -0300

    feat: add nft mint page (#21)

--- lib/app/app_page.dart
+++ lib/app/app_page.dart
@@ -1,17 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:routefly/routefly.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:zup_landing/gen/assets.gen.dart';
+import 'package:zup_landing/routes.g.dart';
 import 'package:zup_ui_kit/buttons/buttons.dart';
 import 'package:zup_ui_kit/zup_colors.dart';
 
-class LandingPage extends StatefulWidget {
-  const LandingPage({super.key});
+class AppPage extends StatefulWidget {
+  const AppPage({super.key});
 
   @override
-  State<LandingPage> createState() => _LandingPageState();
+  State<AppPage> createState() => _AppPageState();
 }
 
-class _LandingPageState extends State<LandingPage> {
+class _AppPageState extends State<AppPage> {
   final double horizontalScreenPadding = 20;
 
   bool isMobile(BoxConstraints constraints) => constraints.maxWidth < 600;
@@ -36,12 +38,28 @@ class _LandingPageState extends State<LandingPage> {
                       Assets.logos.zup.svg(height: 250),
                       const SizedBox(height: 50),
                       ZupPrimaryButton(
+                        title: "Mint Genesis NFT",
+                        border: const BorderSide(color: ZupColors.brand),
+                        backgroundColor: ZupColors.brand,
+                        foregroundColor: ZupColors.white,
+                        onPressed: () => Routefly.navigate(routePaths.mint),
+                        padding: EdgeInsets.zero,
+                        fixedIcon: true,
+                        icon: Assets.logos.zup.svg(height: 20),
+                        width: 200,
+                      ),
+                      const SizedBox(height: 10),
+                      ZupPrimaryButton(
                         title: "Waitlist",
                         onPressed: () => launchUrl(
                           Uri.parse("https://waitlist.zupprotocol.xyz"),
                         ),
                         fixedIcon: true,
-                        width: 150,
+                        hoverElevation: 0,
+                        border: const BorderSide(color: ZupColors.brand),
+                        width: 200,
+                        backgroundColor: Colors.transparent,
+                        foregroundColor: ZupColors.brand,
                         padding: EdgeInsets.zero,
                         icon: Assets.icons.listClipboard.svg(),
                       ),
@@ -59,7 +77,7 @@ class _LandingPageState extends State<LandingPage> {
                         padding: EdgeInsets.zero,
                         fixedIcon: true,
                         icon: Assets.icons.textPage.svg(),
-                        width: 150,
+                        width: 200,
                       ),
                       const SizedBox(height: 24),
                       Row(
@@ -131,21 +149,12 @@ class _LandingPageState extends State<LandingPage> {
             Positioned(
               left: screenWidth * 0.02,
               top: screenWidth * 0.02,
-              child: ShaderMask(
-                shaderCallback: (bounds) => const LinearGradient(
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter,
-                  colors: [ZupColors.brand5, ZupColors.brand],
-                ).createShader(
-                  Rect.fromLTWH(0, 0, bounds.width, bounds.height),
-                ),
-                blendMode: BlendMode.srcIn,
-                child: Text(
-                  "Your very first\nliquidity pool\naggregator",
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: (screenWidth * 0.05).clamp(30, 65),
-                  ),
+              child: Text(
+                "Your very first\nliquidity pool\naggregator",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: ZupColors.brand,
+                  fontSize: (screenWidth * 0.05).clamp(30, 65),
                 ),
               ),
             ),
