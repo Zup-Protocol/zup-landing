9119bd90c8ad412191d19d75c65679b177350046
Ryan Holanda <ryanholanda08@gmail.com>
2025-03-16 10:21:08 -0300

    feat: landing page v2 (#37)

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:routefly/routefly.dart';
 import 'package:url_strategy/url_strategy.dart';
 import 'package:zup_landing/landing_theme.dart';
 import 'package:zup_landing/routes.g.dart';
+import 'package:zup_ui_kit/zup_colors.dart';
 
 void main() {
   setPathUrlStrategy();
@@ -17,16 +18,18 @@ class MainApp extends StatelessWidget {
     return MaterialApp.router(
       theme: LandingTheme.light,
       debugShowCheckedModeBanner: false,
+      color: ZupColors.brand,
       routerConfig: Routefly.routerConfig(
         routes: routes,
-        initialPath: routePaths.path,
+        initialPath: "/",
         routeBuilder: (context, settings, child) => PageRouteBuilder(
-            pageBuilder: (_, __, ___) => child,
-            settings: settings,
-            transitionsBuilder: (_, a, ___, child) => FadeTransition(
-                  opacity: a,
-                  child: child,
-                )),
+          pageBuilder: (_, __, ___) => child,
+          settings: settings,
+          transitionsBuilder: (_, a, ___, child) => FadeTransition(
+            opacity: a,
+            child: SelectionArea(child: child),
+          ),
+        ),
       ),
     );
   }
--- lib/routes.g.dart
+++ lib/routes.g.dart
@@ -2,31 +2,18 @@
 
 import 'package:routefly/routefly.dart';
 
-import 'app/app_page.dart' as a1;
-import 'app/mint/mint_page.dart' as a0;
+import 'app/app_page.dart' as a0;
 
 List<RouteEntity> get routes => [
-      RouteEntity(
-        key: '/mint',
-        uri: Uri.parse('/mint'),
-        routeBuilder: (ctx, settings) => Routefly.defaultRouteBuilder(
-          ctx,
-          settings,
-          const a0.MintNFTPage(),
-        ),
-      ),
       RouteEntity(
         key: '/',
         uri: Uri.parse('/'),
         routeBuilder: (ctx, settings) => Routefly.defaultRouteBuilder(
           ctx,
           settings,
-          const a1.AppPage(),
+          const a0.AppPage(),
         ),
       ),
     ];
 
-const routePaths = (
-  path: '/',
-  mint: '/mint',
-);
+const routePaths = ();
