74653ef749831bd0736eca46245f1a21907845ab
Ryan Holanda <ryanholanda08@gmail.com>
2025-03-16 10:12:49 -0300

    feat: landing page v2 (#34)

--- lib/app/app_page.dart
+++ lib/app/app_page.dart
@@ -1,180 +1,164 @@
 import 'package:flutter/material.dart';
-import 'package:routefly/routefly.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:zup_landing/gen/assets.gen.dart';
-import 'package:zup_landing/routes.g.dart';
+import 'package:zup_landing/widgets/app_header.dart';
 import 'package:zup_ui_kit/buttons/buttons.dart';
 import 'package:zup_ui_kit/zup_colors.dart';
 
-class AppPage extends StatefulWidget {
+class AppPage extends StatelessWidget {
   const AppPage({super.key});
-
-  @override
-  State<AppPage> createState() => _AppPageState();
-}
-
-class _AppPageState extends State<AppPage> {
-  final double horizontalScreenPadding = 20;
-
-  bool isMobile(BoxConstraints constraints) => constraints.maxWidth < 600;
-  bool isHoveringX = false;
-  bool isHoveringTelegram = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        alignment: Alignment.centerLeft,
-        children: [
-          Align(
-            alignment: Alignment.centerLeft,
-            child: Assets.icons.bolt.svg(
-              colorFilter: const ColorFilter.mode(
-                ZupColors.gray6,
-                BlendMode.srcIn,
-              ),
-              height: MediaQuery.sizeOf(context).height,
+      body: LayoutBuilder(builder: (context, constraints) {
+        return CustomScrollView(
+          slivers: [
+            const SliverAppBar(
+              clipBehavior: Clip.none,
+              pinned: true,
+              forceMaterialTransparency: true,
+              title: AppHeader(),
+              toolbarHeight: 80,
+              backgroundColor: Colors.white,
+              surfaceTintColor: Colors.white,
+              elevation: 0,
             ),
-          ),
-          Align(
-            alignment: Alignment.centerLeft,
-            child: Assets.icons.bolt.svg(
-              colorFilter: const ColorFilter.mode(
-                ZupColors.gray6,
-                BlendMode.srcIn,
-              ),
-              height: MediaQuery.sizeOf(context).height,
-            ),
-          ),
-          CustomScrollView(
-            slivers: [
-              SliverPadding(
-                padding: const EdgeInsets.all(20),
-                sliver: SliverFillRemaining(
-                  hasScrollBody: false,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Center(child: Assets.logos.zup.svg(height: 120)),
-                      const SizedBox(height: 20),
-                      Align(
-                        alignment: Alignment.center,
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 65)
-                              .copyWith(top: 0),
-                          child: const Text(
-                            textAlign: TextAlign.center,
-                            "Your very first liquidity pool agreggator",
-                            style: TextStyle(
-                              fontSize: 18,
-                              color: ZupColors.brand,
-                              fontWeight: FontWeight.w600,
+            SliverToBoxAdapter(
+              child: Center(
+                child: AnimatedPadding(
+                  duration: const Duration(milliseconds: 500),
+                  padding: EdgeInsets.symmetric(
+                    horizontal: constraints.maxWidth > 1000 ? 100 : 20,
+                  ).copyWith(bottom: 30, top: 10),
+                  child: Container(
+                    constraints: const BoxConstraints(maxWidth: 1400),
+                    padding: const EdgeInsets.all(40),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(40),
+                      color: ZupColors.brand7,
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          children: [
+                            Expanded(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: [
+                                  FittedBox(
+                                    child: Text(
+                                      "Your Liquidity Pool\nAggregator.",
+                                      // maxLines: 4,
+                                      style: TextStyle(
+                                        color: ZupColors.brand,
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: constraints.maxWidth > 1000
+                                            ? 54
+                                            : 34,
+                                      ),
+                                    ),
+                                  ),
+                                  if (constraints.maxWidth < 600)
+                                    Center(
+                                      child: Assets.images.cryptosExchanges
+                                          .image(width: 400),
+                                    ),
+                                  const SizedBox(height: 30),
+                                  const Text(
+                                    "Discover and Deposit in the Best Liquidity Pool for you",
+                                    style: TextStyle(
+                                        color: ZupColors.black,
+                                        fontSize: 17,
+                                        fontWeight: FontWeight.w500),
+                                  ),
+                                  const Text(
+                                    "In one platform",
+                                    style: TextStyle(
+                                      color: ZupColors.brand,
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  const SizedBox(height: 20),
+                                  ZupPrimaryButton(
+                                    title: "Get Started (Soon)",
+                                    onPressed: null,
+                                    isTrailingIcon: true,
+                                    height: 40,
+                                    fixedIcon: true,
+                                    icon: Assets.icons.arrowRight.svg(),
+                                  ),
+                                  if (constraints.maxWidth > 600)
+                                    const SizedBox(height: 40),
+                                ],
+                              ),
                             ),
+                            if (constraints.maxWidth > 800)
+                              Transform.scale(
+                                scale: 1.3,
+                                child: Assets.images.cryptosExchanges.image(
+                                    height: constraints.maxWidth > 1000
+                                        ? 400
+                                        : 300),
+                              ),
+                          ],
+                        ),
+                        if (constraints.maxWidth > 600)
+                          Assets.icons.brasilArgentinaColors.svg(
+                            fit: BoxFit.fill,
+                          )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            SliverToBoxAdapter(
+              child: Padding(
+                padding: const EdgeInsets.all(50).copyWith(
+                  bottom: 0,
+                ),
+                child: SizedBox(
+                  height: 150,
+                  child: Column(
+                    children: [
+                      const Center(
+                        child: Text(
+                          "Built by Liquidity Providers, for Liquidity Providers.",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: ZupColors.brand,
                           ),
                         ),
                       ),
-                      // const SizedBox(height: 24),
-                      const Row(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [],
-                      ),
-                      const SizedBox(height: 40),
-                      Wrap(
-                        // mainAxisSize: MainAxisSize.min,
-                        runSpacing: 20,
-                        spacing: 20,
-                        alignment: WrapAlignment.center,
-                        crossAxisAlignment: WrapCrossAlignment.center,
-                        children: [
-                          ZupPrimaryButton(
-                            title: "Mint your OG NFT",
-                            border: const BorderSide(color: ZupColors.brand),
-                            backgroundColor: ZupColors.brand,
-                            foregroundColor: ZupColors.white,
-                            onPressed: () => Routefly.navigate(routePaths.mint),
-                            padding: EdgeInsets.zero,
-                            fixedIcon: true,
-                            icon: Assets.icons.bolt.svg(height: 18),
-                            width: 200,
-                          ),
-                          ZupPrimaryButton(
-                            title: "Waitlist",
-                            onPressed: () => launchUrl(
-                              Uri.parse("https://tally.so/r/3yElN0"),
-                            ),
-                            backgroundColor: Colors.transparent,
-                            foregroundColor: ZupColors.brand,
-                            border: const BorderSide(color: ZupColors.brand),
-                            hoverElevation: 0,
-                            fixedIcon: true,
-                            width: 200,
-                            padding: EdgeInsets.zero,
-                            icon: Assets.icons.listClipboard.svg(),
-                          ),
-                          ZupPrimaryButton(
-                            title: "Docs",
-                            hoverElevation: 0,
-                            border: const BorderSide(color: ZupColors.brand),
-                            backgroundColor: Colors.transparent,
-                            foregroundColor: ZupColors.brand,
-                            onPressed: () => launchUrl(
-                              Uri.parse(
-                                  "https://zupprotocol.notion.site/Zup-Documentation-1155a07f86f68059a5e2f2c1cfdf9be2?pvs=4"),
-                            ),
-                            padding: EdgeInsets.zero,
-                            fixedIcon: true,
-                            icon: Assets.icons.textPage.svg(),
-                            width: 200,
-                          ),
-                        ],
-                      ),
                       const SizedBox(height: 30),
                       Row(
-                        mainAxisSize: MainAxisSize.min,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.center,
                         children: [
-                          MouseRegion(
-                            cursor: SystemMouseCursors.click,
-                            onEnter: (event) => setState(
-                              () => isHoveringX = true,
-                            ),
-                            onExit: (event) => setState(
-                              () => isHoveringX = false,
-                            ),
-                            child: GestureDetector(
-                              child: Assets.logos.x.svg(
-                                height: 34,
-                                colorFilter: ColorFilter.mode(
-                                  ZupColors.gray4,
-                                  isHoveringX ? BlendMode.dst : BlendMode.srcIn,
-                                ),
-                              ),
-                              onTap: () => launchUrl(
-                                Uri.parse("https://x.com/zup_protocol"),
-                              ),
+                          ZupLightButton(
+                            child: Assets.logos.x.svg(height: 28),
+                            onPressed: () => launchUrl(
+                              Uri.parse("https://x.com/zup_protocol"),
                             ),
                           ),
-                          const SizedBox(width: 24),
-                          MouseRegion(
-                            cursor: SystemMouseCursors.click,
-                            onEnter: (event) => setState(
-                              () => isHoveringTelegram = true,
+                          const SizedBox(width: 30),
+                          ZupLightButton(
+                            child: Assets.logos.telegram.svg(height: 28),
+                            onPressed: () => launchUrl(
+                              Uri.parse("https://t.me/zupprotocol"),
                             ),
-                            onExit: (event) => setState(
-                              () => isHoveringTelegram = false,
-                            ),
-                            child: GestureDetector(
-                              child: Assets.logos.telegram.svg(
-                                height: 34,
-                                colorFilter: ColorFilter.mode(
-                                  ZupColors.gray4,
-                                  isHoveringTelegram
-                                      ? BlendMode.dst
-                                      : BlendMode.srcIn,
-                                ),
-                              ),
-                              onTap: () => launchUrl(
-                                Uri.parse("https://t.me/zupprotocol"),
-                              ),
+                          ),
+                          const SizedBox(width: 30),
+                          ZupLightButton(
+                            child: Assets.logos.github.svg(height: 28),
+                            onPressed: () => launchUrl(
+                              Uri.parse("https://github.com/zup-protocol"),
                             ),
                           ),
                         ],
@@ -183,19 +167,10 @@ class _AppPageState extends State<AppPage> {
                   ),
                 ),
               ),
-              SliverToBoxAdapter(
-                child: Column(
-                  children: [
-                    const SizedBox(height: 50),
-                    Assets.images.demo.image(height: 400),
-                    const SizedBox(height: 200),
-                  ],
-                ),
-              )
-            ],
-          ),
-        ],
-      ),
+            ),
+          ],
+        );
+      }),
     );
   }
 }
