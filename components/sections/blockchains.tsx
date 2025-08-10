2a6c2aefbe527a1b3390b6043a0685392375214d
Ryan Holanda <ryanholanda08@gmail.com>
2025-08-09 22:48:20 -0300

    fix: icons low quality in mobile

--- components/sections/blockchains.tsx
+++ components/sections/blockchains.tsx
@@ -90,7 +90,7 @@ export default function BlockchainsSection() {
     >
       <div className="w-full max-w-7xl flex flex-col items-center justify-center z-20">
         <motion.div
-          className="h-48 w-full border-b border-b-gray-200 flex flex-col items-center justify-center space-y-2 text-center px-4"
+          className="h-48 w-full flex flex-col items-center justify-center space-y-2 text-center px-4"
           variants={itemVariants}
         >
           <div className="flex flex-col md:flex-col items-center justify-center gap-2">
@@ -102,7 +102,7 @@ export default function BlockchainsSection() {
           <p className="text-2xl text-gray-500">and DEXs</p>
         </motion.div>
 
-        <div className="h-36 w-full border-b border-b-gray-200 flex justify-evenly ">
+        <div className="h-36 w-full flex justify-evenly ">
           <div className="flex flex-row items-center justify-evenly space-x-10">
             {blockchains.map((blockchain) => (
               <motion.div key={blockchain.name} variants={logoVariants}>
@@ -115,11 +115,9 @@ export default function BlockchainsSection() {
               </motion.div>
             ))}
           </div>
-
-          {/* Removed the "More coming soon..." column */}
         </div>
 
-        <div className="h-36 w-full border-b border-b-gray-200 w-screen flex justify-evenly ">
+        <div className="h-36 w-full w-screen flex justify-evenly ">
           <div className="w-1/3 hidden lg:block" />
           <div className="flex items-center justify-center">
             <div className="marquee w-full overflow-hidden">
@@ -146,11 +144,11 @@ export default function BlockchainsSection() {
         </div>
       </div>
 
-      <div className="absolute top-0 left-0 w-full h-full flex flex-col items-center justify-center">
+      {/* <div className="absolute top-0 left-0 w-full h-full flex flex-col items-center justify-center">
         <div className="h-48 w-full border-b border-b-gray-200"></div>
         <div className="h-36 w-full border-b border-b-gray-200"></div>
         <div className="h-36 w-full border-b border-b-gray-200"></div>
-      </div>
+      </div> */}
     </motion.section>
   );
 }
