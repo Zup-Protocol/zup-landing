8f6260d83a4f0d53bf351218151c98d6740d275b
Ryan Holanda <ryanholanda08@gmail.com>
2025-09-01 11:34:00 -0300

    chore: update zup branding (#43)

--- components/ui/navbar.tsx
+++ components/ui/navbar.tsx
@@ -1,26 +1,26 @@
-import Image from 'next/image';
-import Link from 'next/link';
-import Button from './button';
+import Image from "next/image";
+import Link from "next/link";
+import Button from "./button";
 
-import { appUrl } from 'core/constants';
-import logo from 'public/assets/icons/logo.svg';
+import { appUrl } from "core/constants";
+import zupLogo from "public/assets/icons/zupLogo.svg";
 
 const link = [
   {
-    name: 'FAQ',
-    url: 'https://zupprotocol.gitbook.io/documentation/general/faq',
+    name: "FAQ",
+    url: "https://zupprotocol.gitbook.io/documentation/general/faq",
   },
   {
-    name: 'Docs',
-    url: 'https://zupprotocol.gitbook.io/documentation',
+    name: "Docs",
+    url: "https://zupprotocol.gitbook.io/documentation",
   },
-    {
-    name: 'Newsletter',
-    url: 'https://zupprotocol.substack.com/',
+  {
+    name: "Newsletter",
+    url: "https://zupprotocol.substack.com/",
   },
-      {
-    name: 'Contact us',
-    url: 'https://zupprotocol.gitbook.io/documentation/other/contact-us',
+  {
+    name: "Contact us",
+    url: "https://zupprotocol.gitbook.io/documentation/other/contact-us",
   },
 ];
 
@@ -28,30 +28,31 @@ export default function Navbar() {
   return (
     <nav className="w-full flex items-center justify-center p-4">
       <div className="w-full max-w-7xl flex justify-between items-center">
-        <div className='md:w-[33%] flex items-center gap-1 text-[var(--black)]'>
-          <Image src={logo} alt={""} />
-          <span className="text-2xl font-bold">Zup</span>
+        <div className="md:w-[33%] flex items-center gap-1 text-[var(--black)]">
+          <Image
+            src={zupLogo}
+            alt={""}
+            sizes="100%"
+            width={80}
+            height={80}
+            className=""
+          />
         </div>
-        <div className='hidden md:w-[53%] justify-center md:flex items-center gap-10 text-[var(--purple)] font-medium '>
-  {link.map((item, index) => (
-    <Link
-      key={index}
-      href={item.url}
-      passHref
-      legacyBehavior
-    >
-      <a
-        target="_blank"
-        rel="noopener noreferrer"
-        className="flex items-center gap-1 justify-center hover:text-[var(--purple-dark)] hover:font-semibold transition-colors duration-200"
-      >
-        {item.name}
-      </a>
-    </Link>
-  ))}
+        <div className="hidden md:w-[53%] justify-center md:flex items-center gap-10 text-[var(--purple)] font-medium ">
+          {link.map((item, index) => (
+            <Link key={index} href={item.url} passHref legacyBehavior>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                className="flex items-center gap-1 justify-center hover:text-[var(--purple-dark)] hover:font-semibold transition-colors duration-200"
+              >
+                {item.name}
+              </a>
+            </Link>
+          ))}
         </div>
-        <div className='md:w-[33%] flex items-center justify-end'>
-          <Button variant={'primary'} text="Launch App" url={appUrl} />
+        <div className="md:w-[33%] flex items-center justify-end">
+          <Button variant={"primary"} text="Launch App" url={appUrl} />
         </div>
       </div>
     </nav>
